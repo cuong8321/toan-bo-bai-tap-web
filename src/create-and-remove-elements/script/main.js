96e6758813fc17676b3407ca52ea53810206ae27
khai96_ <hvksmr1996@gmail.com>
2017-03-16 21:16:02 +0700

    Update JavaScript

--- src/create-and-remove-elements/script/main.js
+++ src/create-and-remove-elements/script/main.js
@@ -3,18 +3,24 @@ main(window)
 function main (window) {
   'use strict'
   const {document, Image} = window
+  const IMG_CONTAINER_COLS = 4
+  const IMG_SIZE = 256
+  const IMG_SPACING = 4
 
   const imageContainer = document.createElement('div')
 
   document.body.appendChild(imageContainer)
 
-  imageContainer.style.columnCount = 4
+  Object.assign(imageContainer.style, {
+    columnCount: IMG_CONTAINER_COLS,
+    width: IMG_CONTAINER_COLS * IMG_SIZE + (IMG_CONTAINER_COLS + 1) * IMG_SPACING + 'px'
+  })
 
   for (let i = 0; i !== 8; ++i) {
-    const image = new Image(256, 256)
+    const image = new Image(IMG_SIZE, IMG_SIZE)
     const {style} = image
     imageContainer.appendChild(image)
     image.src = `image/${i}.jpg`
-    style.margin = style.padding = '2px'
+    style.margin = style.padding = `${IMG_SPACING >> 1}px`
   }
 }
