695a4dbfb350ef2652f7d07180387247d71772e5
khai96_ <hvksmr1996@gmail.com>
2017-03-17 07:48:09 +0700

    Create a bunch of list items

--- src/list-with-checkboxes/script/main.js
+++ src/list-with-checkboxes/script/main.js
@@ -1,4 +1,50 @@
 'use strict'
 main(window)
 
-function main (window) {}
+function main (window) {
+  const {document, Math: {random}} = window
+  const list = document.getElementById('list')
+
+  addABunchOfListItems()
+
+  function addABunchOfListItems () {
+    return repeatAction(createListItem, 15)
+  }
+
+  function repeatAction (...args) {
+    return Array.from(createCountGenerator(...args))
+  }
+
+  function createListItem () {
+    const li = document.createElement('li')
+    list.appendChild(li)
+    const checkbox = document.createElement('input')
+    li.appendChild(checkbox)
+    checkbox.type = 'checkbox'
+    checkbox.checked = false
+    const span = document.createElement('span')
+    li.appendChild(span)
+    span.textContent = randomString(8)
+    const button = document.createElement('button')
+    li.appendChild(button)
+    button.textContent = 'Delete'
+    return li
+  }
+
+  function * createCountGenerator (callback, count = 0) {
+    for (let i = 0; i !== count; ++i) {
+      yield callback(i, count)
+    }
+  }
+
+  function randomString (count) {
+    return Array
+      .from(createCountGenerator(randomChar, count))
+      .join('')
+  }
+
+  function randomChar () {
+    const table = 'abcdefghijklmnopqrstuvwxyz'
+    return table[parseInt(random() * table.length)]
+  }
+}
