906e1d84ffb9014555039a963121d65f0254707d
khai96_ <hvksmr1996@gmail.com>
2017-03-22 16:28:36 +0700

    Add function search

---
+++ src/filter-and-sort/script/main.js
@@ -0,0 +1,26 @@
+'use strict'
+const searchTextBox = document.getElementById('search-textbox')
+const searchColumn = document.getElementById('search-column')
+const caseSensitiveCheckbox = document.getElementById('case-sensitive-checkbox')
+
+searchTextBox.addEventListener('keydown', filter, false)
+searchTextBox.addEventListener('change', filter, false)
+searchTextBox.addEventListener('paste', filter, false)
+searchColumn.addEventListener('change', filter, false)
+caseSensitiveCheckbox.addEventListener('change', filter, false)
+
+function filter () {
+  const list = Array.from(document.querySelectorAll('table tr'))
+  const getText = caseSensitiveCheckbox.checked
+    ? string => string
+    : string => string.toUpperCase()
+  const text = getText(searchTextBox.value)
+  const column = searchColumn.value
+  const getContentElement = column === 'all'
+    ? row => row
+    : row => row.querySelector('.' + column)
+  list.forEach(row => {
+    const content = getContentElement(row)
+    row.hidden = getText(content.textContent).indexOf(text) === -1
+  })
+}
