515e14839d5964399d5ae6f37fdcffbd5c9d03c4
khai96_ <hvksmr1996@gmail.com>
2017-03-23 14:11:44 +0700

    Complete sort function

--- src/filter-and-sort/script/main.js
+++ src/filter-and-sort/script/main.js
@@ -4,6 +4,7 @@ const searchColumn = document.getElementById('search-column')
 const caseSensitiveCheckbox = document.getElementById('case-sensitive-checkbox')
 const sortOrder = document.getElementById('sort-order')
 const sortColumn = document.getElementById('sort-column')
+const getList = () => Array.from(document.querySelectorAll('table tr'))
 
 searchTextBox.addEventListener('keydown', filter, false)
 searchTextBox.addEventListener('change', filter, false)
@@ -14,8 +15,13 @@ caseSensitiveCheckbox.addEventListener('change', filter, false)
 sortOrder.addEventListener('change', sort, false)
 sortColumn.addEventListener('change', sort, false)
 
+const sortfunc = {
+  ascending: (a, b) => a < b,
+  descending: (a, b) => a > b
+}
+
 function filter () {
-  const list = Array.from(document.querySelectorAll('table tr'))
+  const list = getList()
   const getText = caseSensitiveCheckbox.checked
     ? string => string
     : string => string.toUpperCase()
@@ -28,4 +34,15 @@ function filter () {
     const content = getContentElement(row)
     row.hidden = getText(content.textContent).indexOf(text) === -1
   })
+}
+
+function sort () {
+  const list = getList()
+  const order = sortfunc[sortOrder.value]
+  const column = sortColumn.value
+  const getColumnContent = tr => tr.querySelector('.' + column).textContent
+  const newlist = list.sort((before, after) => order(getColumnContent(before), getColumnContent(after)))
+  const tbody = document.querySelector('table tbody')
+  list.forEach(tr => tr.remove())
+  newlist.forEach(tr => tbody.appendChild(tr))
 }
