2887d5ced549b425238848885bea848b95907e47
khai96_ <hvksmr1996@gmail.com>
2017-03-23 13:46:15 +0700

    Add function sort

--- src/filter-and-sort/script/main.js
+++ src/filter-and-sort/script/main.js
@@ -2,12 +2,17 @@
 const searchTextBox = document.getElementById('search-textbox')
 const searchColumn = document.getElementById('search-column')
 const caseSensitiveCheckbox = document.getElementById('case-sensitive-checkbox')
+const sortOrder = document.getElementById('sort-order')
+const sortColumn = document.getElementById('sort-column')
 
 searchTextBox.addEventListener('keydown', filter, false)
 searchTextBox.addEventListener('change', filter, false)
 searchTextBox.addEventListener('paste', filter, false)
 searchColumn.addEventListener('change', filter, false)
 caseSensitiveCheckbox.addEventListener('change', filter, false)
+
+sortOrder.addEventListener('change', sort, false)
+sortColumn.addEventListener('change', sort, false)
 
 function filter () {
   const list = Array.from(document.querySelectorAll('table tr'))
