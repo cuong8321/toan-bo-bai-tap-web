745cd476f8b1a69a0fe3c2ca174caa9a10b51f89
Cuong <cuong8321@gmail.com>
2017-03-24 07:01:47 +0700

    Comply standard

--- src/cuong/search/script.js
+++ src/cuong/search/script.js
@@ -2,14 +2,14 @@
 const Input = document.getElementById('search-textbox')
 const CheckCase = document.getElementById('thuonghoa')
 Input.addEventListener('change', search, false)
-function search() {
+function search () {
   const array = document.querySelectorAll('li')
 
   const a = CheckCase.checked
   ? string => string
   : string => string.toUpperCase()
   const text = a(Input.value)
-  for(var i of array) {
-        i.hidden=  a(i.textContent).indexOf(text)===-1
-    }
+  for (var i of array) {
+    i.hidden = a(i.textContent).indexOf(text) === -1
   }
+}
