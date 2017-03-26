16e950bb066dd0597410488f6983df4ca7a3f556
Cuong <cuong8321@gmail.com>
2017-03-26 14:22:09 +0700

    Hoàn thành ví dụ

--- src/cuong/Chuyenhoathuong/script.js
+++ src/cuong/Chuyenhoathuong/script.js
@@ -1,9 +1,13 @@
 
 const text = document.getElementById('text')
 const output = document.getElementById('output')
-text.addEventListener('change', result, false)
-function result () {
+
+text.addEventListener('change', tinhKetQua, false)
+tinhKetQua()
+
+function tinhKetQua () {
   const content = Array.from(text.value)
+
   const kitucodau = {
     a: 'áàảãạăắằẳẵặâấầẩẫậ',
     d: 'đ',
@@ -13,15 +17,18 @@ function result () {
     u: 'úùủũụưứừửữự',
     y: 'ýỳỷỹỵ'
   }
+
   for (var i in kitucodau) {
     kitucodau[i.toUpperCase()] = kitucodau[i].toUpperCase()
   }
+
   const chuoidao = []
   for (var j in kitucodau) {
     for (var k of kitucodau[j]) {
       chuoidao[k] = j
     }
   }
+
   for (var x = 0; x < content.length; x++) {
     content[x] = (chuoidao[content[x]] || content[x])
   }
--- src/cuong/search/script.js
+++ src/cuong/search/script.js
@@ -1,15 +1,47 @@
 'use strict'
-const Input = document.getElementById('search-textbox')
-const CheckCase = document.getElementById('thuonghoa')
-Input.addEventListener('change', search, false)
+const input = document.getElementById('search-textbox')
+const checkCase = document.getElementById('thuonghoa')
+input.addEventListener('change', search, false)
+checkCase.addEventListener('change', search, false)
 function search () {
   const array = document.querySelectorAll('li')
 
-  const a = CheckCase.checked
-  ? string => string
-  : string => string.toUpperCase()
-  const text = a(Input.value)
-  for (var i of array) {
-    i.hidden = a(i.textContent).indexOf(text) === -1
+  const kitucodau = {
+    a: 'áàảãạăắằẳẵặâấầẩẫậ',
+    d: 'đ',
+    e: 'éèẻẽẹêếềểễệ',
+    i: 'íìỉĩị',
+    o: 'óòỏõọôốồổỗộơờởỡợ',
+    u: 'úùủũụưứừửữự',
+    y: 'ýỳỷỹỵ'
+  }
+  for (var i in kitucodau) {
+    kitucodau[i.toUpperCase()] = kitucodau[i].toUpperCase()
+  }
+
+  const chuoidao = []
+  for (var j in kitucodau) {
+    for (var k of kitucodau[j]) {
+      chuoidao[k] = j
+    }
+  }
+
+  const a = checkCase.checked
+    ? string => string
+    : string => string.toUpperCase()
+
+  let input1 = Array.from(input.value)
+  for (let y = 0; y < input1.length; y++) {
+    input1[y] = (chuoidao[input1[y]] || input1[y])
+  }
+  const inputText = input1.join('') === input.value ? input1.join('') : input.value
+
+  for (let i of array) {
+    let content = Array.from(i.textContent)
+    for (let x = 0; x < content.length; x++) {
+      content[x] = (chuoidao[content[x]] || content[x])
+    }
+    const text = input1.join('') === input.value ? content.join('') : i.textContent
+    i.hidden = a(text).indexOf(a(inputText)) === -1
   }
 }
